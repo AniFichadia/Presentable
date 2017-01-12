6556d569c643a8b183c898378eefa29a8e321443
Aniruddh Fichadia <Aniruddh.X.Fichadia@nab.com.au>
2017-01-12 20:43:14 +1100

    [Added android resource verification]
    Added ResourceVerifier

---
+++ presentable-android/src/main/java/com/aniruddhfichadia/presentable/ResourceVerifier.java
@@ -0,0 +1,42 @@
+package com.aniruddhfichadia.presentable;
+
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Aniruddh Fichadia
+ * @date 3/1/17
+ */
+public class ResourceVerifier {
+    public static void verifyResourcesPresent(Class<?> androidResFileClass, Class<?> presentableResClass)
+            throws RuntimeException {
+        Field[] declaredFields = presentableResClass.getDeclaredFields();
+
+
+        List<Field> missingFields = new ArrayList<>();
+        for (Field field : declaredFields) {
+            String fieldName = field.getName();
+            try {
+                androidResFileClass.getField(fieldName);
+            } catch (NoSuchFieldException e) {
+                missingFields.add(field);
+            }
+        }
+
+        if (!missingFields.isEmpty()) {
+            StringBuilder sb = new StringBuilder("The following fields are not declared in ")
+                    .append(androidResFileClass.getName());
+
+            for (Field missingField : missingFields) {
+                sb.append('\n');
+                sb.append("\t - ");
+                sb.append(missingField.getName());
+            }
+
+            throw new RuntimeException(sb.toString());
+        }
+    }
+}
